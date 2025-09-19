1714452a6a0c431633404bd663a5eef75009a48e
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-18 20:18:22 -0400

    added mongodb client code

---
+++ src/db.rs
@@ -0,0 +1,19 @@
+﻿
+type URI = String;
+
+pub mod db {
+    use crate::db::{URI};
+
+    use mongodb::{Client, options::ClientOptions};
+
+    pub async fn get_client_uri() -> URI {
+        "mongodb:://localhost:27017".to_owned()
+    }
+
+    pub async fn get_client() -> Client {
+        let db = ClientOptions::parse(get_client_uri().await);
+        let client = Client::with_options(db.await.unwrap()).unwrap();
+
+        client
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::io::stdin;
 use crate::types::GUESS;
 
 mod types;
+mod db;
 
 pub fn get_index_number() -> GUESS {
 
     rand::random_range(1..10)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let guess = get_index_number() as GUESS;
     let mut guesser = String::new();
 
@@ -16,6 +18,14 @@ fn main() {
     let index_guess: i32 = guesser.trim().parse::<i32>().unwrap();
     if index_guess == guess {
         println!("Guess Correct");
+
+        let db = db::db::get_client().await;
+        let dbs = db.database("Guess");
+        dbs.create_collection("GuessedNumber").await.unwrap();
+
+        let coll: mongodb::Collection<String> = dbs.collection("guess");
+        coll.insert_one(guess.to_string()).await.unwrap();
+
     } else {
         println!("Incorrect Guess");
         println!("Guess was: {}", guess);
